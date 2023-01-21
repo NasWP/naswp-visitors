1ec8610309789e9723b96bbdabc9c25313becc3e
Adam Laita <adam@laita.cz>
2023-01-21 17:39:31 +0100

    Update visitors-naswp.php

--- visitors-naswp.php
+++ visitors-naswp.php
@@ -3,7 +3,7 @@
 		Plugin Name: Tester
 		Plugin URI:  https://www.example.com/
 		Description: Tester plugin.
-		Version:     1.0.1
+		Version:     1.0.0
 		Author:      Adam Laita
 		Author URI:  https://www.example.com/
 		License:     GPL3
@@ -11,32 +11,12 @@
 		Text Domain: klen
 	*/
 
-	add_action( 'init', 'github_plugin_updater_test_init' );
-	
-	function github_plugin_updater_test_init() {
-	
+	if( ! class_exists( 'Smashing_Updater' ) ){
 		include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
-	
-		define( 'WP_GITHUB_FORCE_UPDATE', true );
-	
-		if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
-	
-			$config = array(
-				'slug' => plugin_basename( __FILE__ ),
-				'proper_folder_name' => 'visitors-naswp',
-				'api_url' => 'https://api.github.com/repos/adam-laita/visitors-naswp',
-				'raw_url' => 'https://raw.github.com/adam-laita/visitors-naswp/master',
-				'github_url' => 'https://github.com/adam-laita/visitors-naswp',
-				'zip_url' => 'https://github.com/adam-laita/visitors-naswp/archive/master.zip',
-				'sslverify' => true,
-				'requires' => '3.0',
-				'tested' => '3.3',
-				'readme' => 'README.md',
-				'access_token' => '',
-			);
-	
-			new WP_GitHub_Updater( $config );
-	
-		}
-	
 	}
+	
+	$updater = new Smashing_Updater( __FILE__ );
+	$updater->set_username( 'adam-laita' );
+	$updater->set_repository( 'visitors-naswp' );
+	// $updater->authorize( 'abcdefghijk1234567890' );
+	$updater->initialize();
