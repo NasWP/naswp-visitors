812816ada3a2447b39f15ce6a69db1de49688041
Ondřej Šeliga <ondrej@seliga.cz>
2023-03-13 21:36:30 +0100

    Term form meta box.

--- admin/class-naswp-visitors-admin-metabox.php
+++ admin/class-naswp-visitors-admin-metabox.php
@@ -8,25 +8,48 @@ class NasWP_Visitors_Admin_MetaBox
 	/**
 	 * Hook actions.
 	 */
-	public static function hook()
+	public static function hook(): void
 	{
 		$self = new self();
-		add_action( 'add_meta_boxes', array( $self, 'add_meta_box' ) );
-		add_action( 'save_post', array( $self, 'save' ) );
+		add_action( 'add_meta_boxes', [ $self, 'add_post_meta_box' ] );
+		add_action( 'save_post', [ $self, 'save_post' ] );
+
+		$taxonomies = apply_filters( 'naswp_visitors_tax', NASWP_VISITORS_TAX_DEFAULT );
+		foreach ($taxonomies as $tax) {
+			add_action( "{$tax}_edit_form", [ $self, 'add_term_meta_box' ] );
+			add_action( "saved_{$tax}", [ $self, 'save_term' ] );
+		}
+	}
+
+	/**
+	 * Add meta box to defined taxonomy term.
+	 * @param WP_Term $term
+	 */
+	public function add_term_meta_box( WP_Term $term ): void
+	{
+		?>
+		<hr />
+		<?php
+		$this->render_meta_box_content( new NasWP_Visitors_Term( $term->term_id ) );
+		?>
+		<br />
+		<hr />
+		<?php
 	}
 
 	/**
 	 * Add meta box to defined post types.
 	 * @param string $postType
 	 */
-	public function add_meta_box( string $postType )
+	public function add_post_meta_box( string $postType ): void
 	{
 		$cpts = apply_filters( 'naswp_visitors_cpt', NASWP_VISITORS_CPT_DEFAULT );
 		if ( !in_array( $postType, $cpts ) ) return;
 		add_meta_box(
 			'naswp_visitors',
 			__( 'Visitors', 'visitors-naswp' ),
-			[ $this, 'render_meta_box_content' ],
+			// [ $this, 'render_meta_box_content' ],
+			fn( WP_Post $post ) => $this->render_meta_box_content( new NasWP_Visitors_Post( $post->ID ) ),
 			$postType,
 			'side',
 			'high'
@@ -38,9 +61,9 @@ class NasWP_Visitors_Admin_MetaBox
 	 * @param WP_Post $post
 	 * @internal
 	 */
-	public function render_meta_box_content( WP_Post $post )
+	public function render_meta_box_content( NasWP_Visitors_Base $model ): void
 	{
-		$model = new NasWP_Visitors_Post($post->ID);
+		$type = $model instanceof NasWP_Visitors_Post ? __( 'post', 'visitors-naswp' ) : __( 'term', 'visitors-naswp' );
 		?>
 		<div style="display:flex;flex-wrap:wrap;justify-content:space-between;align-items:flex-start;gap:20px;margin-bottom:20px">
 			<?php
@@ -53,25 +76,23 @@ class NasWP_Visitors_Admin_MetaBox
 		<div style="display:flex;flex-wrap:wrap;align-items:center;gap:30px">
 			<label>
 				<br>
-				<input type="checkbox" name="naswp_reset" value="1" class="components-checkbox-control__input">
-				<?php _e( 'Reset all data for this post on next update. Refresh page after save.', 'visitors-naswp' ) ?>
+				<input type="checkbox" name="naswp_reset" value="1">
+				<?php echo sprintf( __( 'Reset all data for this %s on next update. Refresh page after save.', 'visitors-naswp' ), $type ) ?>
 			</label>
 
 			<div class="form-field form-required term-name-wrap">
 				<label for="naswp_total"><?php _e( 'Total views number', 'visitors-naswp' ) ?></label>
 				<input name="naswp_total" id="naswp_total" type="number" value="<?php echo $model->get_total() ?>" size="40" aria-required="true">
 			</div>
-
-			<?php wp_nonce_field( 'visitors-naswp', 'naswp_visitors_nonce' ) ?>
 		</div>
 		<?php
 	}
 
 	/**
 	 * Render table of total views.
-	 * @param NasWP_Visitors_Post $model
+	 * @param NasWP_Visitors_Base $model
 	 */
-	private function render_float_table(NasWP_Visitors_Post $model)
+	private function render_float_table( NasWP_Visitors_Base $model ): void
 	{
 		?>
 		<table style="flex-shrink:0">
@@ -104,13 +125,13 @@ class NasWP_Visitors_Admin_MetaBox
 
 	/**
 	 * Render views per time interval in table.
-	 * @param NasWP_Visitors_Post $model
+	 * @param NasWP_Visitors_Base $model
 	 * @param string $title
 	 * @param array|array[] $views
 	 * @param string $dateFormat
 	 * @internal
 	 */
-	private function render_views_table( NasWP_Visitors_Post $model, string $title, array $views, string $dateFormat )
+	private function render_views_table( NasWP_Visitors_Base $model, string $title, array $views, string $dateFormat ): void
 	{
 		?>
 		<table style="flex-shrink:0">
@@ -132,37 +153,47 @@ class NasWP_Visitors_Admin_MetaBox
 	}
 
 	/**
-	 * Save metabox data.
+	 * Save post metabox data.
 	 * @param int $postId
+	 * @return int
 	 */
-	public function save( int $postId )
+	public function save_post( int $postId ): int
 	{
-
-		if ( !isset( $_POST['naswp_visitors_nonce'] ) ) {
-			return $postId;
-		}
-		$nonce = $_POST['naswp_visitors_nonce'];
-		if ( ! wp_verify_nonce( $nonce, 'naswp_visitors' ) ) {
-			return $postId;
-		}
-
 		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
 			return $postId;
 		}
 
-
-		$model = new NasWP_Visitors_Post($postId);
+		$model = new NasWP_Visitors_Post( $postId );
 		if ( isset( $_POST['naswp_reset'] ) && $_POST['naswp_reset'] ) {
 			$model->reset_data();
 		}
 
 		$total = filter_var( $_POST['naswp_total'], FILTER_SANITIZE_NUMBER_INT );
-		$model->update_total($total);
+		$model->update_total( $total );
 
 		if ( $model->get_last_update() === 0 ) {
 			$model->reset_last_update();
 		}
 
 		return $postId;
+	}
+
+	/**
+	 * Save term metabox data.
+	 * @param int $termId
+	 */
+	public function save_term( int $termId ): void
+	{
+		$model = new NasWP_Visitors_Term( $termId );
+		if ( isset( $_POST['naswp_reset'] ) && $_POST['naswp_reset'] ) {
+			$model->reset_data();
+		}
+
+		$total = filter_var( $_POST['naswp_total'], FILTER_SANITIZE_NUMBER_INT );
+		$model->update_total( $total );
+
+		if ( $model->get_last_update() === 0 ) {
+			$model->reset_last_update();
+		}
 	}
 }
